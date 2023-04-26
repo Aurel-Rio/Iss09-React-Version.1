b0ef5725b3045fd427f9d77e7114f1e0713d777d
Aureli-Rio <servant.aurelien.developer@gmail.com>
2023-04-26 14:07:16 +0200

    Iss09 ReactJs v0.1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,7 @@ import React from "react";
 import '../css/navbar.css';
 import logo_navbar from "../img/iss09_logo_navbar.png";
 import { Link } from "react-router-dom";
-import Accueil from './Accueil';
-import Forfaits from './Forfaits';
-import Assistance from './Assistance';
-import Depannage from './Depannage';
-import Deplacement from './Deplacement';
-import Contact from './Contact';
+
 
 const Navbar = () => {
     const logoNavbar = logo_navbar;
@@ -16,12 +11,12 @@ const Navbar = () => {
             <nav>
                 <img id="logo_navbar_css" src={logoNavbar} alt="Logo de l'entreprise iss09"></img>
                 <ul>
-                    <li><Link to="#">Accueil</Link></li>
-                    <li><Link to="#">Forfaits</Link></li>
-                    <li><Link to="#">Assistance</Link></li>
-                    <li><Link to="#">Dépannage</Link></li>
-                    <li><Link to="#">Déplacement</Link></li>
-                    <li><Link to="#">Contact</Link></li>
+                    <li><Link to="/">Accueil</Link></li>
+                    <li><Link to="/forfaits">Forfaits</Link></li>
+                    <li><Link to="assistance">Assistance</Link></li>
+                    <li><Link to="depannage">Dépannage</Link></li>
+                    <li><Link to="deplacement">Déplacement</Link></li>
+                    <li><Link to="contact">Contact</Link></li>
                 </ul>
             </nav>
         </header>
